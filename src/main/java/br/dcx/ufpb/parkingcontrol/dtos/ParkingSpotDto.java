0f6eb2e82af3247a70e0bda15013d5df502f5a07
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-01 20:09:42 -0300

    Feat: Implements a getters and setters methods

--- src/main/java/br/dcx/ufpb/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/br/dcx/ufpb/parkingcontrol/dtos/ParkingSpotDto.java
@@ -29,4 +29,69 @@ public class ParkingSpotDto {
 
   @NotBlank
   private String block;  
+
+  //gerar todos os metodos getters e setters
+  public String getParkingSpotNumber() {
+    return parkingSpotNumber;
+  }
+
+  public void setParkingSpotNumber(String parkingSpotNumber) {
+    this.parkingSpotNumber = parkingSpotNumber;
+  }
+
+  public String getLicensePlateCar() {
+    return licensePlateCar;
+  }
+
+  public void setLicensePlateCar(String licensePlateCar) {
+    this.licensePlateCar = licensePlateCar;
+  }
+
+  public String getBrandCar() {
+    return brandCar;
+  }
+
+  public void setBrandCar(String brandCar) {
+    this.brandCar = brandCar;
+  }
+
+  public String getModelCar() {
+    return modelCar;
+  }
+
+  public void setModelCar(String modelCar) {
+    this.modelCar = modelCar;
+  }
+
+  public String getColorCar() {
+    return colorCar;
+  }
+
+  public void setColorCar(String colorCar) {
+    this.colorCar = colorCar;
+  }
+
+  public String getResponsibleName() {
+    return responsibleName;
+  }
+
+  public void setResponsibleName(String responsibleName) {
+    this.responsibleName = responsibleName;
+  }
+
+  public String getApartament() {
+    return apartament;
+  }
+
+  public void setApartament(String apartament) {
+    this.apartament = apartament;
+  }
+
+  public String getBlock() {
+    return block;
+  }
+
+  public void setBlock(String block) {
+    this.block = block;
+  }
 }
