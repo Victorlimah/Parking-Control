d1118030ea27d76b315e44853194917486a5c1fb
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-01 19:50:12 -0300

    Feat: Implements entity ParkingSpotModel

---
+++ src/test/java/br/dcx/ufpb/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dcx.ufpb.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
