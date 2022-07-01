d7b20ce862fddce76b4c589c3be2961f73465745
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-01 20:24:32 -0300

    Feat: Implements a post method and save data in BD

--- src/main/java/br/dcx/ufpb/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/br/dcx/ufpb/parkingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,20 @@
 package br.dcx.ufpb.parkingcontrol.services;
 
-import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.dcx.ufpb.parkingcontrol.dtos.ParkingSpotDto;
+import br.dcx.ufpb.parkingcontrol.models.ParkingSpotModel;
 import br.dcx.ufpb.parkingcontrol.repositories.ParkingSpotRepository;
 
 
@@ -12,6 +25,21 @@ public class ParkingSpotService {
 
   public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
     this.parkingSpotRepository = parkingSpotRepository;
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+    var parkingSpotModel = new ParkingSpotModel();
+
+    BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+
+    parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+    return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotRepository.save(parkingSpotModel));
+  }
+
+  @Transactional
+  public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+    return parkingSpotRepository.save(parkingSpotModel);
   }
 
 }
