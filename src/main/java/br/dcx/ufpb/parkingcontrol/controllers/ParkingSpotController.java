97690b1353165a0cb7c61f4a074e38ec38391ef6
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-01 23:05:11 -0300

    Feat: Implements method put a register

--- src/main/java/br/dcx/ufpb/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/br/dcx/ufpb/parkingcontrol/controllers/ParkingSpotController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -83,5 +84,22 @@ public class ParkingSpotController {
 
     parkingSpotService.delete(parkingSpotModelOptional.get());
     return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully");
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateParkingSpot (@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+    Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+
+    if(!parkingSpotModelOptional.isPresent()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found: Parking Spot not found");
+    }
+
+    var parkingSpotModel = parkingSpotModelOptional.get();
+    BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+
+    parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+    parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+
+    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
   }
 }
